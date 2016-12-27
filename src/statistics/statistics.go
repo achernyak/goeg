890362b7ce0f2f9d8e4497e40568cd58533fab34
Artem Chernyak <artemchernyak@gmail.com>
2016-12-27 07:47:02 -0600

    more stats

--- src/statistics/statistics.go
+++ src/statistics/statistics.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"sort"
 	"strconv"
@@ -30,6 +31,8 @@ type statistics struct {
 	numbers []float64
 	mean    float64
 	median  float64
+	mode    []float64
+	stdDev  float64
 }
 
 func main() {
@@ -80,7 +83,10 @@ func formatStats(stats statistics) string {
 	<tr><td>Count</td><td>%d</td></tr>
 	<tr><td>Mean</td><td>%f</td></tr>
 	<tr><td>Median</td><td>%f</td></tr>
-	</table>`, stats.numbers, len(stats.numbers), stats.mean, stats.median)
+	<tr><td>Mode</td><td>%f</td></tr>
+	<tr><td>StdDiv</td><td>%f</td></tr>
+	</table>`, stats.numbers, len(stats.numbers), stats.mean, stats.median,
+		stats.mode, stats.stdDev)
 }
 
 func getStats(numbers []float64) (stats statistics) {
@@ -88,6 +94,8 @@ func getStats(numbers []float64) (stats statistics) {
 	sort.Float64s(stats.numbers)
 	stats.mean = sum(numbers) / float64(len(numbers))
 	stats.median = median(numbers)
+	stats.mode = mode(numbers)
+	stats.stdDev = stdDev(numbers, stats.mean)
 	return stats
 }
 
@@ -105,4 +113,34 @@ func median(numbers []float64) float64 {
 		result = (result + numbers[middle-1]) / 2
 	}
 	return result
+}
+
+func mode(numbers []float64) (modes []float64) {
+	frequencies := make(map[float64]int, len(numbers))
+	highestFrequency := 0
+	for _, x := range numbers {
+		frequencies[x]++
+		if frequencies[x] > highestFrequency {
+			highestFrequency = frequencies[x]
+		}
+	}
+	for x, frequency := range frequencies {
+		if frequency == highestFrequency {
+			modes = append(modes, x)
+		}
+	}
+	if highestFrequency == 1 || len(modes) == len(numbers) {
+		modes = modes[:0]
+	}
+	sort.Float64s(modes)
+	return modes
+}
+
+func stdDev(numbers []float64, mean float64) float64 {
+	total := 0.0
+	for _, number := range numbers {
+		total += math.Pow(number-mean, 2)
+	}
+	variance := total / float64(len(numbers)-1)
+	return math.Sqrt(variance)
 }
