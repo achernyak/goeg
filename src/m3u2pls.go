47b050e8a169a15388c0d6b547359326ed7cacc9
Artem Chernyak <artemchernyak@gmail.com>
2017-01-01 11:22:14 -0600

    finished implementation

--- src/m3u2pls.go
+++ src/m3u2pls.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -40,11 +41,47 @@ func readM3uPlaylist(data string) (songs []Song) {
 			song.Title, song.Seconds = parseExtinfLine(line)
 		} else {
 			song.Filename = strings.Map(mapPlatformDirSeparator, line)
+			fmt.Println("vim-go")
 		}
-		if song.Filename != "" && song.title != "" && song.Seconds != 0 {
+		if song.Filename != "" && song.Title != "" && song.Seconds != 0 {
 			songs = append(songs, song)
 			song = Song{}
 		}
 	}
 	return songs
+}
+
+func parseExtinfLine(line string) (title string, seconds int) {
+	if i := strings.IndexAny(line, "-0123456789"); i > -1 {
+		const separator = ","
+		line = line[i:]
+		if j := strings.Index(line, separator); j > -1 {
+			title = line[j+len(separator):]
+			var err error
+			if seconds, err = strconv.Atoi(line[:j]); err != nil {
+				log.Printf("failed to read teh duration for '%s': %v\n",
+					title, err)
+				seconds = -1
+			}
+		}
+	}
+	return title, seconds
+}
+
+func mapPlatformDirSeparator(char rune) rune {
+	if char == '/' || char == '\\' {
+		return filepath.Separator
+	}
+	return char
+}
+
+func writePlsPlaylist(songs []Song) {
+	fmt.Println("[playlist]")
+	for i, song := range songs {
+		i++
+		fmt.Printf("File%d=%s\n", i, song.Filename)
+		fmt.Printf("Title%d=%s\n", i, song.Title)
+		fmt.Printf("Length%d=%d\n", i, song.Seconds)
+	}
+	fmt.Printf("NumberOfEntries=%d\nVersion=2\n", len(songs))
 }
