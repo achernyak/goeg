35633e3ec553cfe5a47e5d47bb53658ce3e46cd1
Artem Chernyak <artemchernyak@gmail.com>
2017-01-06 06:58:46 -0600

    reading in file

--- src/guess_separator/guess_separator.go
+++ src/guess_separator/guess_separator.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -18,4 +21,29 @@ func main() {
 	counts := createCounts(lines, separators, linesRead)
 	separator := guessSep(counts, separators, linesRead)
 	report(separator)
+}
+
+func readUpToNLines(filename string, maxLines int) (int, []string) {
+	var file *os.File
+	var err error
+	if file, err = os.Open(filename); err != nil {
+		log.Fatal("failed to open the file: ", err)
+	}
+	defer file.Close()
+	lines := make([]string, maxLines)
+	reader := bufio.NewReader(file)
+	i := 0
+	for ; i < maxLines; i++ {
+		line, err := reader.ReadString('\n')
+		if line != "" {
+			lines[i] = line
+		}
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal("failed to finish reading the file: ", err)
+		}
+	}
+	return i, lines[:i]
 }
