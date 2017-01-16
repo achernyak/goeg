2832cef606aabc99a4db60d16d28e03ba0fd82e9
Artem Chernyak <artemchernyak@gmail.com>
2017-01-16 06:59:12 -0600

    exported functions

--- src/shapes1/shapes/shapes.go
+++ src/shapes1/shapes/shapes.go
@@ -1,12 +1,17 @@
 package shapes
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
+	"image/jpeg"
+	"image/png"
 	"log"
+	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 var saneLength, saneRadius, saneSides func(int) int
@@ -50,4 +55,29 @@ func FillImage(width, height int, fill color.Color) draw.Image {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(img, img.Bounds(), &image.Uniform{fill}, image.ZP, draw.Src)
 	return img
+}
+
+func DrawShapes(img draw.Image, x, y int, shapes ...Shaper) error {
+	for _, shape := range shapes {
+		if err := shape.Draw(img, x, y); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func SaveImage(img image.Image, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	switch strings.ToLower(filepath.Ext(filename)) {
+	case ".jpg", ".jpeg":
+		return jpeg.Encode(file, img, nil)
+	case ".png":
+		return png.Encode(file, img)
+	}
+	return fmt.Errorf("shapes.SaveImage(): '%s' has an unrecognized "+
+		"suffix", filename)
 }
