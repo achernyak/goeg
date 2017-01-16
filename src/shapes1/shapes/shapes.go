db427878b5655a54f2935b6df935f4a393677ab4
Artem Chernyak <artemchernyak@gmail.com>
2017-01-16 06:49:15 -0600

    basic drawing function

--- src/shapes1/shapes/shapes.go
+++ src/shapes1/shapes/shapes.go
@@ -4,7 +4,42 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"log"
+	"path/filepath"
+	"runtime"
 )
+
+var saneLength, saneRadius, saneSides func(int) int
+
+func init() {
+	saneLength = makeBoundedIntFunc(1, 4096)
+	saneRadius = makeBoundedIntFunc(1, 1024)
+	saneSides = makeBoundedIntFunc(3, 60)
+}
+
+func makeBoundedIntFunc(minimum, maximum int) func(int) int {
+	return func(x int) int {
+		valid := x
+		switch {
+		case x < minimum:
+			valid = minimum
+		case x > maximum:
+			valid = maximum
+		}
+		if valid != x {
+			log.Printf("%s(): replace %d with %d\n", caller(1), x, valid)
+		}
+		return valid
+	}
+}
+
+func caller(steps int) string {
+	name := "?"
+	if pc, _, _, ok := runtime.Caller(steps + 1); ok {
+		name = filepath.Base(runtime.FuncForPC(pc).Name())
+	}
+	return name
+}
 
 func FillImage(width, height int, fill color.Color) draw.Image {
 	if fill == nil {
