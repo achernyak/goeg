6338b12266731263f8813342e82b69352d05af1b
Artem Chernyak <artemchernyak@gmail.com>
2017-01-20 08:02:23 -0600

    constructors

--- src/omap/omap.go
+++ src/omap/omap.go
@@ -1,5 +1,7 @@
 package omap
 
+import "strings"
+
 type Map struct {
 	root   *node
 	less   func(interface{}, interface{}) bool
@@ -10,4 +12,32 @@ type node struct {
 	key, value  interface{}
 	red         bool
 	left, right *node
+}
+
+func New(less func(interface{}, interface{}) bool) *Map {
+	return &Map{less: less}
+}
+
+func NewCaseFoldedKeyed() *Map {
+	return &Map{less: func(a, b interface{}) bool {
+		return strings.ToLower(a.(string)) < strings.ToLower(b.(string))
+	}}
+}
+
+func NewStringKeyed() *Map {
+	return &Map{less: func(a, b interface{}) bool {
+		return a.(string) < b.(string)
+	}}
+}
+
+func NewIntKeyed() *Map {
+	return &Map{less: func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}}
+}
+
+func NewFloat64Keyed() *Map {
+	return &Map{less: func(a, b interface{}) bool {
+		return a.(float64) < b.(float64)
+	}}
 }
