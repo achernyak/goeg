3856208ff93d35cfa1cf477384d24fb5b8c13711
Artem Chernyak <artemchernyak@gmail.com>
2017-01-23 07:32:02 -0600

    started cgrep1

---
+++ src/cgrep1/cgrep.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+)
+
+var workers = runtime.NumCPU()
+
+type Result struct {
+	filename string
+	lino     int
+	line     string
+}
+
+type Job struct {
+	filename string
+	results  chan<- Result
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	if len(os.Args) < 3 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <regexp> <files>\n",
+			filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	if lineRx, err := regexp.Compile(os.Args[1]); err != nil {
+		log.Fatalf("invalid regexp: %s\n", err)
+	} else {
+		grep(lineRx, commandLineFiles(os.Args[2:]))
+	}
+}
