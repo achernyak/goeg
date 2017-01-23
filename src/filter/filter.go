5e42388720430b2e5aa75032d28e70fa862a8dbd
Artem Chernyak <artemchernyak@gmail.com>
2017-01-22 20:49:04 -0600

    filter functions

--- src/filter/filter.go
+++ src/filter/filter.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	log.SetFlags(0)
-	algrightm,
+	algorithm,
 		minSize, maxSize, suffixes, files := handleCommandLine()
 
 	if algorithm == 1 {
@@ -18,5 +22,66 @@ func main() {
 		channel2 := filterSuffixes(suffixes, channel1)
 		channel3 := filterSize(minSize, maxSize, channel2)
 		sink(channel3)
+	}
+}
+
+func source(files []string) <-chan string {
+	out := make(chan string, 1000)
+	go func() {
+		for _, filename := range files {
+			out <- filename
+		}
+		close(out)
+	}()
+	return out
+}
+
+func filterSuffixes(suffixes []string, in <-chan string) <-chan string {
+	out := make(chan string, cap(in))
+	go func() {
+		for filename := range in {
+			if len(suffixes) == 0 {
+				out <- filename
+				continue
+			}
+			ext := strings.ToLower(filepath.Ext(filename))
+			for _, suffix := range suffixes {
+				if ext == suffix {
+					out <- filename
+					break
+				}
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func filterSize(minimum, maximum int64, in <-chan string) <-chan string {
+	out := make(chan string, cap(in))
+	go func() {
+		for filename := range in {
+			if minimum == -1 && maximum == -1 {
+				out <- filename
+				continue
+			}
+			finfo, err := os.Stat(filename)
+			if err != nil {
+				continue
+			}
+			size := finfo.Size()
+			if (minimum == -1 || minimum > -1 && minimum <= size) &&
+				(maximum == -1 || maximum > -1 && maximum >= size) {
+				out <- filename
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sink(in <-chan string) {
+	for filename := range in {
+		fmt.Println(filename)
 	}
 }
