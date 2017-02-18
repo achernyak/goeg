3a1f5ce817a8a998078468866c94bd09a41da559
Artem Chernyak <artemchernyak@gmail.com>
2017-02-18 12:22:13 -0600

    implemented concurency functions

--- src/cgrep/cgrep.go
+++ src/cgrep/cgrep.go
@@ -40,7 +40,7 @@ var workers = runtime.NumCPU()
 
 func grep(lineRx *regexp.Regexp, filenames []string) {
 	jobs := make(chan Job, workers)
-	results := make(chan Result, minimum(1000, len(filename)))
+	results := make(chan Result, minimum(1000, len(filenames)))
 	done := make(chan struct{}, workers)
 
 	go addJobs(jobs, filenames, results)
@@ -49,4 +49,31 @@ func grep(lineRx *regexp.Regexp, filenames []string) {
 	}
 	go awaitCompletion(done, results)
 	processResults(results)
+}
+
+func addJobs(jobs chan<- Job, filenames []string, results chan<- Result) {
+	for _, filename := range filenames {
+		jobs <- Job{filename, results}
+	}
+	close(jobs)
+}
+
+func doJobs(done chan<- struct{}, lineRx *regexp.Regexp, jobs <-chan Job) {
+	for job := range jobs {
+		job.Do(lineRx)
+	}
+	done <- struct{}{}
+}
+
+func awaitCompletion(done <-chan struct{}, results chan Result) {
+	for i := 0; i < workers; i++ {
+		<-done
+	}
+	close(results)
+}
+
+func processResults(results <-chan Result) {
+	for result := range results {
+		fmt.Printf("%s:%d:%s\n", result.filename, result.lino, result.line)
+	}
 }
