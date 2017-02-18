f3e45b3e24464ecb149a40087e4e4c7163634a9c
Artem Chernyak <artemchernyak@gmail.com>
2017-02-18 12:41:56 -0600

    finished cgrep

--- src/cgrep/cgrep.go
+++ src/cgrep/cgrep.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -20,6 +23,29 @@ type Result struct {
 type Job struct {
 	filename string
 	results  chan<- Result
+}
+
+func (job Job) Do(lineRx *regexp.Regexp) {
+	file, err := os.Open(job.filename)
+	if err != nil {
+		log.Printf("errors: %s\n", err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for lino := 1; ; lino++ {
+		line, err := reader.ReadBytes('\n')
+		line = bytes.TrimRight(line, "\n\r")
+		if lineRx.Match(line) {
+			job.results <- Result{job.filename, lino, string(line)}
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("error:%d: %s\n", lino, err)
+			}
+			break
+		}
+	}
 }
 
 func main() {
