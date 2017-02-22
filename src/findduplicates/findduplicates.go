2bda5f035594b14d3088725736ca753aa5b98f35
Artem Chernyak <artemchernyak@gmail.com>
2017-02-22 07:01:25 -0600

    walking functions

--- src/findduplicates/findduplicates.go
+++ src/findduplicates/findduplicates.go
@@ -5,7 +5,14 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
 )
+
+type fileInfo struct {
+	sha1 []byte
+	size int64
+	path string
+}
 
 const maxGoroutines = 100
 
@@ -20,4 +27,31 @@ func main() {
 	go findDuplicates(infoChan, os.Args[1])
 	pathData := mergeResults(infoChan)
 	outputResults(pathData)
+}
+
+func findDuplicates(infoChan chan fileInfo, dirname string) {
+	waiter := &sync.WaitGroup{}
+	filepath.Walk(dirname, makeWalkFunc(infoChan, waiter))
+	waiter.Wait()
+	close(infoChan)
+}
+
+const maxSizeOfSmallFile = 1024 * 32
+
+func makeWalkFunc(infoChan chan fileInfo,
+	waiter *sync.WaitGroup) func(string, os.FileInfo, error) error {
+	return func(path string, info os.FileInfo, err error) error {
+		if err == nil && info.Size() > 0 &&
+			(info.Mode()&os.ModeType == 0) {
+			if info.Size() < maxSizeOfSmallFile ||
+				runtime.NumGoroutine() > maxGoroutines {
+				processFile(path, info, infoChan, nil)
+			} else {
+				writer.Add(1)
+				go processFile(path, info, infoChan,
+					func() { waiter.Done() })
+			}
+		}
+		return nil
+	}
 }
