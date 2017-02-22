4af39f41bb0c7649c311a9089e4dde39686d910a
Artem Chernyak <artemchernyak@gmail.com>
2017-02-22 06:22:49 -0600

    processing lines

--- src/apachereport/apachereport.go
+++ src/apachereport/apachereport.go
@@ -1,6 +1,16 @@
 package main
 
-import "runtime"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"safemap"
+)
 
 var workers = runtime.NumCPU()
 
@@ -17,4 +27,47 @@ func main() {
 	processLines(done, pageMap, lines)
 	waitUntil(done)
 	showResults(pageMap)
+}
+
+func readLines(filename string, lines chan<- string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("failed to open the file:", err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if line != "" {
+			lines <- line
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Println("failed to finish reading the file:", err)
+			}
+			break
+		}
+	}
+	close(lines)
+}
+
+func processLines(done chan<- struct{}, pageMap safemap.SafeMap,
+	lines <-chan string) {
+	getRx := regexp.MustCompile(`GET[ \t]+([^ \t\n]+[.]html?)`)
+	incrementer := func(value interface{}, found bool) interface{} {
+		if found {
+			return value.(int) + 1
+		}
+		return 1
+	}
+	for i := 0; i < workers; i++ {
+		go func() {
+			for line := range lines {
+				if matches := getRx.FindStringSubmatch(line); matches != nil {
+					pageMap.Update(matches[1], incrementer)
+				}
+			}
+			done <- struct{}{}
+		}()
+	}
 }
