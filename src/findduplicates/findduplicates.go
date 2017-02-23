7d044ea771fa8afd5db007cfbd1e2736dd901efd
Artem Chernyak <artemchernyak@gmail.com>
2017-02-23 07:47:11 -0600

    processing functions

--- src/findduplicates/findduplicates.go
+++ src/findduplicates/findduplicates.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sort"
 	"sync"
 )
 
@@ -15,6 +16,11 @@ type fileInfo struct {
 	sha1 []byte
 	size int64
 	path string
+}
+
+type pathsInfo struct {
+	size  int64
+	paths []string
 }
 
 const maxGoroutines = 100
@@ -80,4 +86,38 @@ func processFile(filename string, info os.FileInfo,
 		return
 	}
 	infoChan <- fileInfo{hash.Sum(nil), info.Size(), filename}
+}
+
+func mergeResults(infoChan <-chan fileInfo) map[string]*pathsInfo {
+	pathData := make(map[string]*pathsInfo)
+	format := fmt.Sprintf("%%016X:%%%dX", sha1.Size*2)
+	for info := range infoChan {
+		key := fmt.Sprintf(format, info.size, info.sha1)
+		value, found := pathData[key]
+		if !found {
+			value = &pathsInfo{size: info.size}
+			pathData[key] = value
+		}
+		value.paths = append(value.paths, info.path)
+	}
+	return pathData
+}
+
+func outputResults(pathData map[string]*pathsInfo) {
+	keys := make([]string, 0, len(pathData))
+	for key := range pathData {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		value := pathData[key]
+		if len(value.paths) > 1 {
+			fmt.Printf("%d duplicate files (%s byes):\n",
+				len(value.paths), commas(value.size))
+			sort.Strings(value.paths)
+			for _, name := range value.paths {
+				fmt.Printf("\t%s\n", name)
+			}
+		}
+	}
 }
