eefdc5ef15a402e65dcd1cb0fd2c886820a38116
Artem Chernyak <artemchernyak@gmail.com>
2017-02-25 09:46:56 -0600

    processing file

--- src/imagetag/imagetag.go
+++ src/imagetag/imagetag.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"os"
 	"path/filepath"
 	"runtime"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 )
 
 var workers = runtime.NumCPU()
@@ -26,4 +31,59 @@ func main() {
 		go doJobs(done, results, jobs)
 	}
 	waitAndProcessResults(done, results)
+}
+
+func addJobs(files []string, jobs chan<- string) {
+	for _, filename := range files {
+		jobs <- filename
+	}
+	close(jobs)
+}
+
+func doJobs(done chan<- struct{}, results chan<- string,
+	jobs <-chan string) {
+	for job := range jobs {
+		if result, ok := process(job); ok {
+			results <- result
+		}
+	}
+	done <- struct{}{}
+}
+
+func waitAndProcessResults(done <-chan struct{}, results <-chan string) {
+	for working := workers; working > 0; {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-done:
+			working--
+		}
+	}
+DONE:
+	for {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		default:
+			break DONE
+		}
+	}
+}
+
+func process(filename string) (string, bool) {
+	if info, err := os.Stat(filename); err != nil ||
+		(info.Mode()&os.ModeType == 1) {
+		return "", false
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", false
+	}
+	defer file.Close()
+	config, _, err := image.DecodeConfig(file)
+	if err != nil {
+		return "", false
+	}
+	return fmt.Sprintf(`<img src="%s" width="%d" height="%d" />`,
+		filepath.Base(filename), config.Width, config.Height), true
 }
