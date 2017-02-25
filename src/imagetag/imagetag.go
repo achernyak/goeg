e1f61a0e57fabc2906d7d8bead3e1e95964b6da1
Artem Chernyak <artemchernyak@gmail.com>
2017-02-25 09:26:02 -0600

    started imagetag

--- src/imagetag/imagetag.go
+++ src/imagetag/imagetag.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+var workers = runtime.NumCPU()
 
 func main() {
-	fmt.Println("vim-go")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <image files>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	files := commandLineFiles(os.Args[1:])
+	jobs := make(chan string, workers*16)
+	results := make(chan string)
+	done := make(chan struct{}, workers)
+
+	go addJobs(files, jobs)
+	for i := 0; i < workers; i++ {
+		go doJobs(done, results, jobs)
+	}
+	waitAndProcessResults(done, results)
 }
