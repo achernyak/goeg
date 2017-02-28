b9bc701acd1718bdd2967dc9b8d2b5690a1d951c
Artem Chernyak <artemchernyak@gmail.com>
2017-02-28 08:09:17 -0600

    unpack zipped

--- src/unpack/unpack.go
+++ src/unpack/unpack.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -37,4 +39,49 @@ func unpackArchive(filename string) error {
 		return unpackZip(filename)
 	}
 	return unpackTar(filename)
+}
+
+func unpackZip(filename string) (err error) {
+	var reader *zip.ReadCloser
+	if reader, err = zip.OpenReader(filename); err != nil {
+		return err
+	}
+	defer reader.Close()
+	for _, zipFile := range reader.Reader.File {
+		filename := sanitizedName(zipFile.Name)
+		if strings.HasSuffix(zipFile.Name, "/") ||
+			strings.HasSuffix(zipFile.Name, "\\") {
+			if err = os.MkdirAll(filename, 0755); err != nil {
+				return err
+			}
+
+		} else {
+			if err = unpackZippedFile(filename, zipFile); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func unpackZippedFile(filename string, zipFile *zip.File) error {
+	writer, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+	reader, err := zipFile.Open()
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	if _, err = io.Copy(writer, reader); err != nil {
+		return err
+	}
+	if filename == zipFile.Name {
+		fmt.Println(filename)
+	} else {
+		fmt.Printf("%s [%s]\n", filename, zipFile.Name)
+	}
+	return nil
 }
