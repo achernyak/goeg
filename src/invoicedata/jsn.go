d6d74e4a2f16e9dccbb3a7fad4eef0e35e059189
Artem Chernyak <artemchernyak@gmail.com>
2017-03-01 07:29:56 -0600

    unmarshal json

--- src/invoicedata/jsn.go
+++ src/invoicedata/jsn.go
@@ -1,6 +1,9 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 var version int
 
@@ -37,4 +40,46 @@ func (invoice Invoice) MarshalJSON() ([]byte, error) {
 		invoice.Items,
 	}
 	return json.Marshal(jsonInvoice)
+}
+
+func (invoice *Invoice) UnmarshalJSON(data []byte) (err error) {
+	var jsonInvoice JSONInvoice
+	if version == fileVersion {
+		if err = json.Unmarshal(data, &jsonInvoice); err != nil {
+			return err
+		}
+	} else {
+		var jsonInvoice100 JSONInvoice100
+		if err = json.Unmarshal(data, &jsonInvoice100); err != nil {
+			return err
+		}
+		jsonInvoice = JSONInvoice{
+			jsonInvoice100.Id,
+			jsonInvoice100.CustomerId,
+			"",
+			jsonInvoice100.Raised,
+			jsonInvoice100.Due,
+			jsonInvoice100.Paid,
+			jsonInvoice100.Note,
+			jsonInvoice100.Items,
+		}
+	}
+	var raised, due time.Time
+	if raised, err = time.Parse(dateFormat, jsonInvoice.Raised); err != nil {
+		return err
+	}
+	if due, err = time.Parse(dateFormat, jsonInvoice.Due); err != nil {
+		return err
+	}
+	*invoice = Invoice{
+		jsonInvoice.Id,
+		jsonInvoice.CustomerId,
+		jsonInvoice.DepartmentId,
+		raised,
+		due,
+		jsonInvoice.Paid,
+		jsonInvoice.Note,
+		jsonInvoice.Items,
+	}
+	return nil
 }
