7a90d8b02125fa3d1271f837e949352bdd9420a5
Artem Chernyak <artemchernyak@gmail.com>
2017-03-03 07:24:56 -0600

    linkutil

--- src/linkcheck/linkutil/linkutil.go
+++ src/linkcheck/linkutil/linkutil.go
@@ -1,9 +1,45 @@
 package linkutil
 
-import "regexp"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
 
 var hrefRx *regexp.Regexp
 
 func init() {
 	hrefRx = regexp.MustCompile(`<a[^>]+href=['"]?([^'">]+)['"]?`)
+}
+
+func LinksFromURL(url string) ([]string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get page: %s", err)
+	}
+	links, err := LinksFromReader(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse page: %s", err)
+	}
+	return links, nil
+}
+
+func LinksFromReader(reader io.Reader) ([]string, error) {
+	html, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	uniqueLinks := make(map[string]bool)
+	for _, submatch := range hrefRx.FindAllSubmatch(html, -1) {
+		uniqueLinks[string(submatch[1])] = true
+	}
+	links := make([]string, len(uniqueLinks))
+	i := 0
+	for link := range uniqueLinks {
+		links[i] = link
+		i++
+	}
+	return links, nil
 }
